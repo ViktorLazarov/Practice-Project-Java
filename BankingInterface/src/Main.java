87fdc103d7860f2692f980467dc9bed1ac1199de
Viktor Lazarov <viktorlazarov@abv.bg>
2024-08-19 14:35:26 +0200

    added setters and getters for the user class, rebuilt the code to use the User class instead of 2D ArrayList

--- BankingInterface/src/Main.java
+++ BankingInterface/src/Main.java
@@ -6,34 +6,23 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner scanner = new Scanner(System.in);
-		String userChoice = "";
-		ArrayList<ArrayList<String>> users = new ArrayList<ArrayList<String>>();
-		boolean exit = false;
-		
-//		User user1 = new User("viktor1", "lazarov1", 100);
-//		
-//		System.out.println(user1.getName());
-//		
-//		user1.setName("viktor");
-//		
-//		System.out.println(user1.getName());
-		
+		String userChoice;
+		ArrayList<User> users = new ArrayList<>();
+		boolean exit = false;		
 		
 		while(!exit) {
 			userChoice = takeInput(scanner);
 			
-			while(!userChoice.equals("1") && !userChoice.equals("2") && !userChoice.equals("3")) {
-				
-				System.out.println("Please enter a valid option");
-				userChoice = takeInput(scanner);
-			}
-			
 			switch (userChoice) {
 			case "1":
 				// log in function
-				printing('*', 30);
-				if(login(users, scanner)) {
-					System.out.println("Logged in!");
+				String loginStatus = login(users, scanner);
+				printing('*', 30); // print a line with 30 "*"
+
+				if(loginStatus.equals("logged in")) { 			// checks if the user entered an existing username and then it checks if the 
+					System.out.println("Logged in!"); // entered password is correct
+				} else if(loginStatus.equals("canceled")){
+					System.out.println("Operation was canceled");
 				} else {
 					System.out.println("No user found, please register first.");
 				}
@@ -44,11 +33,16 @@ public class Main {
 				printing('*', 30);
 				System.out.println("Create User");
 				printing('*', 30);
-				createUser(users, scanner);
+				createUser(users, scanner); //create a user and insert it into the 'users' arrayList
 				printing('*', 30);
 				break;
 			case "3":
+				//exit the program
 				exit = true;
+				break;
+			default:
+				//the user entered something different than the awaited input ("1", "2" or "3")
+				System.out.println("Please enter a valid option");
 				break;
 			}
 		}
@@ -75,44 +69,50 @@ public class Main {
 		return scanner.nextLine();
 	}
 	
-	private static void createUser(ArrayList<ArrayList<String>> users, Scanner scanner) {
-		ArrayList<String> newUser = new ArrayList<String>();
-	
+	private static void createUser(ArrayList<User> users, Scanner scanner) {	
 		System.out.print("Enter a new username: ");
-		String username = scanner.nextLine();	
-		newUser.add(username);
+		String username = scanner.nextLine();
+		User newUser = new User();
+		newUser.setName(username);
 		
 		System.out.print("Enter a password: ");
 		String password = scanner.nextLine();
-		newUser.add(password);
+		newUser.setPassword(password);
 		
 		users.add(newUser);
 		
-		System.out.printf("User \"%s\" created!\n", username);
+		System.out.printf("User \"%s\" created!\n", newUser.getName());
 		
 	}
 	
-	private static boolean login(ArrayList<ArrayList<String>> users, Scanner scanner) {
+	private static String login(ArrayList<User> users, Scanner scanner) {
 		System.out.print("Please enter your username: ");
 		String username = scanner.nextLine();
+		String responce = "";
 		
 		
-		for(ArrayList<String> user : users) {
-			if(user.contains(username)) {
+		for(User user : users) {
+			if(user.getName().equals(username)) {
 				System.out.print("Please enter your password: ");
-				String password = scanner.nextLine();
-				if(user.contains(password)) {
-					return true;
-					
-				} else {
-					System.out.println("Wrong Password!");
+				while (true) {
+					String password = scanner.nextLine();
+					if(user.getPassword().equals(password)) {
+						responce = "logged in";
+						return responce;
+					} else if(password.equals("cancel%%")){
+						responce = "canceled";
+						return responce;
+					} 
+					else {
+						System.out.println("Wrong password,please try again.(type 'cancel%%' to cancel the operation)");
+						System.out.print("Please enter your password: ");
+					}
 				}
-			}else {
-				System.out.println("The user doesnt exist");
-				
+			} else {
+				responce = "does not exist";
 			}
 		}
-		return false;
-		
+
+		return responce;
 	}
 }
--- BankingInterface/src/User.java
+++ BankingInterface/src/User.java
@@ -1,32 +1,28 @@
 public class User {
 	private String name;
 	private String password;
-	private double balance;
-	
-	// constructor
-	public User(String name, String password , double balance){
-		this.name = name;
-		this.password = password;
-		this.balance = balance;
-		
-	}
-	
-	public User(String name, String password){
-		this.name = name;
-		this.password = password;
-		
-	}
-	
-	
-	
+	private double balance = 0;
+
 	//setter
 	public void setName(String name) {
 		this.name = name;
+	}
+	public void setPassword(String password){
+		this.password = password;
+	}
+	public void setBalance(double amount){
+		this.balance = amount;
 	}
 	
 	//getter
 	public String getName() {
 		return this.name;
+	}
+	public String getPassword() {
+		return this.password;
+	}
+	public double getBalance() {
+		return this.balance;
 	}
 	
 }
